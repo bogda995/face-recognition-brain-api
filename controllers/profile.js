c0a45fc57aa9794a73e586900a24189d71d54e1f
bogda995 <56070240+bogda995@users.noreply.github.com>
2022-05-10 17:28:08 +0300

    Innitial commit

---
+++ controllers/profile.js
@@ -0,0 +1,20 @@
+const handleProfileGet = (req, res, db) => {
+	const {id} = req.params;
+	let found = false;
+	db.select('*').from('users').where({id})
+		.then(user => {
+			console.log(user)
+			if (user.length) {
+				res.json(user[0]);
+			} else {
+				res.status(400).json('Not Found')
+			}
+		}).catch(err => res.status(400).json('Error Getting User'))
+// if (!found) {
+// 	res.status(400).json('not found');
+// }
+}
+
+module.exports = {
+	handleProfileGet
+}
---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		port: 5432,
+		user: 'postgres',
+		password: 'test',
+		database: 'postgres'
+	}
+});
+
+//knex database test
+// db.select('*').from('users').then(data => {
+// 	console.log(data);
+// })
+// db.select('*')
+// 	.from('users').then(data => {
+// 	console.log(data);
+// });
+
+const app = express();
+
+// local database
+// const database = {
+// 	users: [
+// 		{
+// 			id: '123',
+// 			name: 'Johnny',
+// 			email: 'CaptainJohnny@email.com',
+// 			password: 'rom',
+// 			entries: 0,
+// 			joined: new Date()
+// 		},
+// 		{
+// 			id: '124',
+// 			name: 'William',
+// 			email: 'william@gmail.com',
+// 			password: 'heart',
+// 			entries: 0,
+// 			joined: new Date()
+// 		}
+// 	]
+// }
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('success');
+})
+
+//Dependency injection
+app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)})
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000')
+})
